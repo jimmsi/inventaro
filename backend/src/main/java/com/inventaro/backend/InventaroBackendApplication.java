3449a8c0626d413c33aa5c19bce48d418971a755
Jimmy Emanuelsson <jimmyem@gmail.com>
2025-09-10 10:49:01 +0200

    build(api): Initialize Spring Boot project with H2 and base structure
    Configured H2 as in-memory database and added dependencies for
    Spring Web and Spring Data JPA. Established initial project
    package structure including controllers, services, repositories,
    and models.
    
    Solves #1

---
+++ backend/src/main/java/com/inventaro/backend/InventaroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inventaro.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventaroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventaroBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/inventaro/backend/InventaroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventaro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventaroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
