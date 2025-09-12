bb89442d17e3eedd4792098a2c14d3ba42a2ab96
Jimmy Emanuelsson <jimmyem@gmail.com>
2025-09-12 10:56:44 +0200

    chore(api): configure Hibernate to auto-create schema and load seed data

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO article (id, name, quantity, unit, low_stock_threshold) VALUES
+                                                                        (RANDOM_UUID(), 'Munskydd', 200, 'st', 100),
+                                                                        (RANDOM_UUID(), 'Handskar', 50, 'box', 200),
+                                                                        (RANDOM_UUID(), 'Termometrar', 1, 'st', 2),
+                                                                        (RANDOM_UUID(), 'Sprutor', 300, 'st', 100),
+                                                                        (RANDOM_UUID(), 'Desinfektionsmedel', 20, 'flaska', 10),
+                                                                        (RANDOM_UUID(), 'Plåster', 500, 'st', 100);
